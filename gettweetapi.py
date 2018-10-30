ae41b7b0a4b214e38cfaa9227ef8e5d27998ad08
Onur Yılmaz <42644185+devonuryilmaz@users.noreply.github.com>
2018-10-31 02:29:06 +0300

    Update gettweetapi.py

--- gettweetapi.py
+++ gettweetapi.py
@@ -2,7 +2,7 @@ import requests
 
 from bs4 import BeautifulSoup
 
-url = "http://www.twitter.com/onuuryiilmaz"
+url = "http://www.twitter.com/" #+username
 
 response = requests.get(url)
 
@@ -11,14 +11,11 @@ html_icerik = response.content
 soup = BeautifulSoup(html_icerik,"html.parser")
 
 basliklar = soup.find_all("p",{"class":"TweetTextSize TweetTextSize--normal js-tweet-text tweet-text"})
-i = 0
 
 for baslik in basliklar:
-    if i==50:
-        break
     baslik = baslik.text
 
     baslik= baslik.strip()
     baslik = baslik.replace("\n","")
     print(baslik)
-    i+=1
+    
